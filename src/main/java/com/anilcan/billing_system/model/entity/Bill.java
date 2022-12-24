0dff8b564336fae23e7edf4b12daca71e9425e9f
Anil Can OZGOK <ozgok1903@gmail.com>
2022-12-25 02:41:50 +0300

    Bill model created.

--- src/main/java/com/anilcan/billing_system/model/entity/Bill.java
+++ src/main/java/com/anilcan/billing_system/model/entity/Bill.java
@@ -1,14 +1,15 @@
-package com.anilcan.billing_system.model;
+package com.anilcan.billing_system.model.entity;
 
-import lombok.NoArgsConstructor;
+import lombok.Builder;
 
-@NoArgsConstructor
+@Builder
 public record Bill(
         Long billNo,
         String firstName,
         String lastName,
         String email,
         double amount,
-        String productName) { }
+        String productName,
+        boolean billStatus) { }
 
 //NOTE: records are immutable therefore it doesn't have setters
