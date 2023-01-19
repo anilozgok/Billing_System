f7d8aec0a7704cdf6937fa66b84fc679fbffeb7e
Anil Can OZGOK <ozgok1903@gmail.com>
2023-01-20 01:32:14 +0300

    bill record changed to the lombok

--- src/main/java/com/anilcan/billing_system/model/entity/Bill.java
+++ src/main/java/com/anilcan/billing_system/model/entity/Bill.java
@@ -1,13 +1,29 @@
 package com.anilcan.billing_system.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record Bill(
-        Long billNo,
-        String firstName,
-        String lastName,
-        String email,
-        double amount,
-        String productName,
-        boolean billStatus) { }
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "Bill")
+@Entity
+@NamedQuery(name = "Bill.getRejectedBill", query = "SELECT b FROM Bill b where b.billStatus=false")
+@NamedQuery(name = "Bill.getAcceptedBill", query = "SELECT b FROM Bill b where b.billStatus=true")
+public class Bill {
 
-//NOTE: records are immutable therefore it doesn't have setters
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long billNo;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private double amount;
+    private String productName;
+    private boolean billStatus;
+    private boolean shipment;
+}
